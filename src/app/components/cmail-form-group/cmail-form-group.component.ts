fc6e38cbdf40476f2ffe8a804edca3bc8710f37a
meiregoncalves <m_hgoncalves@hotmail.com>
2020-02-15 12:02:09 -0300

    Aula 3.1

--- src/app/components/cmail-form-group/cmail-form-group.component.ts
+++ src/app/components/cmail-form-group/cmail-form-group.component.ts
@@ -5,16 +5,19 @@ import { Component, ElementRef } from '@angular/core';
   templateUrl: './cmail-form-group.component.html'
 })
 export class CmailFormFieldComponent {
-  valorDaLabel = '';
+  valorDaLabel = "";
+  idDaLabel = "";
 
   ngOnInit() {
     const inputDaTela = this.elementRef.nativeElement;
 
     const placeholder = inputDaTela.querySelector('input').getAttribute('placeholder');
     this.valorDaLabel = placeholder;
-
     console.log(this.valorDaLabel);
-    inputDaTela.querySelector('input').setAttribute('placeholder', ' ');
+    inputDaTela.querySelector('input').setAttribute('placeholder', ' ');    
+
+    this.idDaLabel = inputDaTela.querySelector('input').getAttribute('name');
+    inputDaTela.querySelector('input').setAttribute('id', this.idDaLabel);  
   }
 
   constructor (private elementRef: ElementRef){
