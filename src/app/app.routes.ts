b30010b407a4f6ccf6b0e2ecf2c358bc80d6bc5e
meiregoncalves <m_hgoncalves@hotmail.com>
2020-03-07 10:09:23 -0300

    Aula 5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
 import { CmailCadastroComponent } from './modules/cadastro/cadastro.component';
 import { CmailInboxComponent } from './modules/inbox/inbox.component';
+import { LoginComponent } from './modules/login/login.component';
 
 const rotas: Routes = [
     { path: '', component: CmailInboxComponent },
     { path: 'cadastro', component: CmailCadastroComponent },
+    { path: 'login', component: LoginComponent },
     { path: '**', redirectTo: '' }
 ]
 
---
+++ src/app/modules/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+    selector:'login-component',
+    templateUrl: './login.component.html',
+})
+
+export class LoginComponent {
+    titulo: string = 'Página de login';
+    formLogin = new FormGroup({
+        login : new FormControl('', [Validators.email, Validators.required]),
+        senha : new FormControl('', [Validators.required])
+    })
+    handleLoginUsuario() {
+        console.log('Login em desenvolvimento');
+    }
+}
